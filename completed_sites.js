f41670ca32a2d970b5286b3e41bc89efe203c7d2
kmaetschke <kathim@eoas.ubc.ca>
2026-08-15 14:12:05 -0700

    Update runningSites in completed_sites.js

--- completed_sites.js
+++ completed_sites.js
@@ -5,8 +5,10 @@
 
 //  var runningSites = [545, 6144];
 
-var runningSites = [];
-
+var runningSites = [672, 650, 655, 
+                    656, 666, 675, 
+                    674, 652, 676, 
+                    678, 622, 653];
 
 var completedSites = [
 
