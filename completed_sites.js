883a8cf842df145f7ea08a198fa464ed60674c90
kmaetschke <kathim@eoas.ubc.ca>
2026-08-17 00:08:46 -0700

    Update completed_sites.js

--- completed_sites.js
+++ completed_sites.js
@@ -5,11 +5,13 @@
 
 //  var runningSites = [545, 6144];
 
-var runningSites = [572, 550, 555, 
-                    556, 566, 575, 
-                    574, 552, 576, 
-                    578, 522, 553];
+var runningSites = [574, 552, 576, 
+                    578, 522, 553,
+                    560, 562, 558,
+                    570, 563, 580
+];
 
-var completedSites = [
+var completedSites = [555, 566, 575,
+                      572, 550, 556
 
 ];
